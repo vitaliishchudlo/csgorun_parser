12804e8ad5f950af5bce303e631d5d64551b1483
Vitalii <vitalik.shchudlo01@ukr.net>
2021-03-04 15:37:43 +0200

    file synchronization

--- main.py
+++ main.py
@@ -7,28 +7,43 @@ import os
 import plyer.platforms.win.notification
 from plyer import notification
 from ast import literal_eval
+from pathlib import Path
+
+
 
 koef = float(input('ЯКИЙ КОФ МЕНШЕ? (X.X) '))  # ask what coefficient to check
 driver = webdriver.Chrome(executable_path='chromedriver.exe')  # open browser Chrome
-# driver.minimize_window()
+# driver.minimize_window() # Скрити вікно
 driver.get('https://csgorun.pro/')  # open site CS:GO RUN
 
 list = [10 ** 10, 10 ** 10, 10 ** 10]  # history of crash
 
 
 def writer_file(num):
-    
     stat = {'2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0}
     stat[str(num)] = 1
-
-    f = open('new.txt', 'r')
+    f = open('statistic.txt', 'r')
     file_stat = literal_eval(f.read())
     z = {k: stat[k] + file_stat[k] for k in stat}
     f.close()
-    w = open('new.txt', 'w')
+    w = open('statistic.txt', 'w')
     print(z)
     w.write(str(z))
     w.close()
+
+
+
+def check_file_statistic(num):
+    my_file = Path("statistic.txt")
+    if my_file.is_file() == True:
+        print(my_file.is_file())
+        writer_file(num)
+    else:
+        print('FALSE')
+        file = open("statistic.txt", "w")
+        file.write(str("{'2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0}"))
+        file.close()
+        check_file_statistic(num)
 
 
 while True:
@@ -51,7 +66,7 @@ while True:
                 float(list[6]) < float(koef) and float(list[7]) < float(koef):
             message = f'ВИПАЛО 8 РАЗ МЕНШЕ {koef}'
             notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(8)
+            check_file_statistic(8)
 
         elif float(list[0]) < float(koef) and float(list[1]) < float(koef) and \
                 float(list[2]) < float(koef) and float(list[3]) < float(koef) and \
@@ -59,38 +74,38 @@ while True:
                 float(list[6]) < float(koef):
             message = f'ВИПАЛО 7 РАЗ МЕНШЕ {koef}'
             notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(7)
+            check_file_statistic(7)
 
         elif float(list[0]) < float(koef) and float(list[1]) < float(koef) and \
                 float(list[2]) < float(koef) and float(list[3]) < float(koef) and \
                 float(list[4]) < float(koef) and float(list[5]) < float(koef):
             message = f'ВИПАЛО 6 РАЗ МЕНШЕ {koef}'
             notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(6)
+            check_file_statistic(6)
 
         elif float(list[0]) < float(koef) and float(list[1]) < float(koef) and \
                 float(list[2]) < float(koef) and float(list[3]) < float(koef) and \
                 float(list[4]) < float(koef):
             message = f'ВИПАЛО 5 РАЗ МЕНШЕ {koef}'
-            # notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(5)
+            notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
+            check_file_statistic(5)
 
         elif float(list[0]) < float(koef) and float(list[1]) < float(koef) and \
                 float(list[2]) < float(koef) and float(list[3]) < float(koef):
             message = f'ВИПАЛО 4 РАЗИ МЕНШЕ {koef}'
-            # notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(4)
+            notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
+            check_file_statistic(4)
 
         elif float(list[0]) < float(koef) and float(list[1]) < float(koef) and \
                 float(list[2]) < float(koef):
             message = f'ВИПАЛО 3 РАЗИ МЕНШЕ {koef}'
-            # notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(3)
+            notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
+            check_file_statistic(3)
 
         elif float(list[0]) < float(koef) and float(list[1]) < float(koef):
             message = f'ВИПАЛО 2 РАЗИ МЕНШЕ {koef}'
-            # notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
-            writer_file(2)
+            notification.notify(title="CS GO RUN HACK", message=message, app_name='HACKER')
+            check_file_statistic(2)
 
         else:
             print('More')
